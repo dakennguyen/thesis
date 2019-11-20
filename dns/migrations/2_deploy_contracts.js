2d557815e659c3ae5a4a28c282a5c8264a312f64
Khoa Nguyen <ngdoan.anhkhoa@gmail.com>
2019-11-20 16:19:01 +0700

    WIP

--- dns/migrations/2_deploy_contracts.js
+++ dns/migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 const Services = artifacts.require("Services");
+const Types = artifacts.require("Types");
 
 module.exports = function(deployer) {
-  deployer.deploy(Services);
+    deployer.deploy(Services);
+    deployer.deploy(Types);
 };
